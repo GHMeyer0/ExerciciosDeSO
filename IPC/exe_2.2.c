23f96f782113ef0be0df10c0f2f7ccc28c1a66da
Gabriel Helko Meyer <ghmeyer0@gmail.com>
2019-10-22 19:59:59 -0300

    Adicionnado laçõ de teste

--- IPC/exe_2.2.c
+++ IPC/exe_2.2.c
@@ -9,8 +9,7 @@ sem_t lock, lock_two;
 
 void  X(void *argp) {
 	n = n * 16;
-	sem_post(&lock);
-	sem_wait(&lock_two);
+	sem_post(&lock); // libera o primeiro sinal
 }
 void Z(void *argp) {
 	sem_wait(&lock);
@@ -18,19 +17,20 @@ void Z(void *argp) {
 	sem_post(&lock_two);
 }
 void Y(void *argp) {
-	sem_wait(&lock);
 	sem_wait(&lock_two);
 	n = n / 7;
 }
 
 int main(void) {
-	int i = 0;
-	int contador = 0;
-	while (i < 10000)
+	int quantidades_de_testes = 1000;
+	int resultados_incorretos = 0;
+	
+	// Laço para testar o programa na quantidade de vezes que foi definida na variavel "quantidades_de_testes"
+	for (size_t i = 0; i < quantidades_de_testes; i++)
 	{
 		pthread_t t1, t2, t3;
-		sem_init(&lock, 0, 1);
-		sem_init(&lock_two, 0, 1);
+		sem_init(&lock, 0, 0); // Inicializa primeiro sinal travado
+		sem_init(&lock_two, 0, 0); // Inicializa segundo sinal travado
 
 		int rc;
 		rc = pthread_create(&t1, NULL, (void *)X, NULL);
@@ -43,12 +43,11 @@ int main(void) {
 		printf("n=%d\n", n);
 		if (n != 8)
 		{
-			contador++;
+			resultados_incorretos++;
 		}
 		n = 1;
-		i++;
 	}
-	printf("contador=%d\n", contador);
+	printf("contador de erros = %d\n", resultados_incorretos);
 	
 	
 	return 0;
